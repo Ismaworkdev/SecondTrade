abe9b68f92283e53ee8455a71500a702c476b8c7
ismaworkdev <ismaelworkdv@gmail.com>
2025-04-16 18:13:47 +0200

    fix router

--- Api/app.py
+++ Api/app.py
@@ -1,22 +1,12 @@
 from fastapi import FastAPI
 from config.db import engine, metaDatos
-from routes.Usuario import Usuario
-from routes.Producto import Producto
-from routes.ProductoFavorito import ProductoFavorito
-from routes.UsuarioFavorito import UsuarioFavorito
-from routes.ImagenProducto import ImagenProducto
-from routes.Conversacion import Conversacion
-from routes.Mensaje import Mensaje
+from routes import router
+
 app = FastAPI()
 #metaDatos.drop_all(bind=engine)
 #metaDatos.create_all(bind=engine)
-app.include_router(Usuario)
-app.include_router(Producto)
-app.include_router(ProductoFavorito)
-app.include_router(UsuarioFavorito)
-app.include_router(ImagenProducto)
-app.include_router(Conversacion)
-app.include_router(Mensaje)
+app.include_router(router)
+
 
 @app.get("/")
 def root():
--- Api/routes/__init__.py
+++ Api/routes/__init__.py
@@ -1,5 +1,11 @@
-from .Conversacion import *
-from .Usuario import *
-from .Mensaje import *
-from ImagenProducto import *
-from .Producto import *
+from fastapi import APIRouter
+from Api.routes import Usuario , Producto , Conversacion , Mensaje 
+
+router = APIRouter()
+
+router.include_router(Usuario.router, prefix="/usuario", tags=["Usuario"])
+router.include_router(Producto.router, prefix="/producto", tags=["Producto"])
+router.include_router(Conversacion.router, prefix="/conversacion", tags=["Conversacion"])
+router.include_router(Mensaje.router, prefix="/mensaje", tags=["Mensaje"])
+
+__all__ = ["router"]
